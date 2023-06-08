7ac371d0ac90a08733f63619cfd79e7b81d058de
shaniihiiew <115487591+shaniihiiew@users.noreply.github.com>
2023-06-09 02:15:58 +0800

    Update handler.go

--- rpc-server/handler.go
+++ rpc-server/handler.go
@@ -1,71 +1,115 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"math/rand"
-	"sort"
-	"strings"
-
-	"github.com/TikTokTechImmersion/assignment_demo_2023/rpc-server/kitex_gen/rpc"
+    "context"
+    "encoding/json"
+    "github.com/go-redis/redis"
 )
 
-type IMServiceImpl struct{}
+type IMServiceImpl struct {
+    redisClient *redis.Client
+}
+
+func NewIMServiceImpl(redisClient *redis.Client) *IMServiceImpl {
+    return &IMServiceImpl{
+        redisClient: redisClient,
+    }
+}
 
 func (s *IMServiceImpl) Send(ctx context.Context, req *rpc.SendRequest) (*rpc.SendResponse, error) {
-	import (
-    client := redis.NewClient(&redis.Options{
-        Addr:     "your-redis-address:6379",
-        Password: "your-redis-password", 
-        DB:       0,                     
-    })
 
-    pong, err := client.Ping().Result()
-    if err != nil {
-        return err
-    }
-    fmt.Println(pong) 
-
-    err = client.Set("your-key", "your-value", 0).Err()
-    if err != nil {
-        return err
+    message := &Message{
+        Sender    string `json:"sender"`
+	Message   string `json:"message"`
+	Timestamp int64  `json:"timestamp"`
     }
 
-    // Request sent successfully
-    fmt.Println("Send request executed successfully")
-    return nil
-	}
-	resp := rpc.NewSendResponse()
-	resp.Code, resp.Messages = 0, "success"
-	return resp, nil
-}
-	
-}
+    roomID := getRoomID(req.GetChat())
+    if roomID == "" {
+        return nil, fmt.Errorf("invalid Chat ID '%s', should be in the format of user1:user2", req.GetChat())
+    }
 
-func (s *IMServiceImpl) Pull(ctx context.Context, req *rpc.PullRequest) (*rpc.PullResponse, error) {
-
-func pullMessages(start, end int64) ([]string, error) {
-    client := redis.NewClient(&redis.Options{
-        Addr:     "your-redis-address:6379",
-        Password: "your-redis-password", 
-        DB:       0,                    
-    })
-    result, err := client.ZRange("your-messages-key", start, end).Result()
+    err := s.SaveMessage(ctx, roomID, message)
     if err != nil {
         return nil, err
     }
-    return result, nil
+
+    resp := &rpc.SendResponse{
+        Code: 0, 
+        Msg:  "success",
+    }
+
+    return resp, nil
 }
 
-	resp := rpc.NewPullResponse()
-	resp.Code, resp.Messages = 0, "success"
-	return resp, nil
+func (s *IMServiceImpl) Pull(ctx context.Context, req *rpc.PullRequest) (*rpc.PullResponse, error) {
+    roomID := getRoomID(req.GetChat())
+    if roomID == "" {
+        return nil, fmt.Errorf("invalid Chat ID '%s', should be in the format of user1:user2", req.GetChat())
+    }
+
+    start := req.GetCursor()
+    end := start + int64(req.GetLimit())
+
+    messages, err := s.GetMessagesByRoomID(ctx, roomID, start, end, req.GetReverse())
+    if err != nil {
+        return nil, err
+    }
+
+    respMessages := make([]*rpc.Message, 0)
+    var counter int32 = 0
+    var nextCursor int64 = 0
+    hasMore := false
+    for _, msg := range messages {
+        if counter+1 > req.GetLimit() {
+            hasMore = true
+            nextCursor = end
+            break
+        }
+        temp := &rpc.Message{
+            Chat:     req.GetChat(),
+            Text:     msg.Message,
+            Sender:   msg.Sender,
+            SendTime: msg.Timestamp,
+        }
+        respMessages = append(respMessages, temp)
+        counter += 1
+    }
+
+    resp := &rpc.PullResponse{
+        Messages:    respMessages,
+        Code:        0,
+        Msg:         "success",
+        HasMore:     &hasMore,
+        NextCursor:  &nextCursor,
+    }
+
+    return resp, nil
 }
 
-func areYouLucky() (int32, string) {
-	if rand.Int31n(2) == 1 {
-		return 0, "success"
-	} else {
-		return 500, "oops"
-	}
+func (s *IMServiceImpl) SaveMessage(ctx context.Context, roomID string, message *Message) error {
+    text, err := json.Marshal(message)
+    if err != nil {
+        return err
+    }
+
+    member := &redis.Z{
+        Score:  float64(message.Timestamp),
+        Member: text,
+    }
+
+    _, err = s.redisClient.ZAdd(ctx, roomID, member).Result()
+    if err != nil {
+        return err
+    }
+
+    return nil
 }
+
+func (s *IMServiceImpl) GetMessagesByRoomID(ctx context.Context, roomID string, start, end int64, reverse bool) ([]*Message, error) {
+    var rawMessages []string
+
+    if reverse {
+        rawMessages, err := s.redisClient.ZRevRange(ctx, roomID, start, end).Result()
+        if err != nil
+
