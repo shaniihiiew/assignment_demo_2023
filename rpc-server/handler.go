f5671cb2a75191ac15a8b94671457d9e7055ddce
shaniihiiew <115487591+shaniihiiew@users.noreply.github.com>
2023-06-09 01:49:13 +0800

    Update handler.go

--- rpc-server/handler.go
+++ rpc-server/handler.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"math/rand"
+	"sort"
+	"strings"
 
 	"github.com/TikTokTechImmersion/assignment_demo_2023/rpc-server/kitex_gen/rpc"
 )
@@ -11,14 +14,62 @@ import (
 type IMServiceImpl struct{}
 
 func (s *IMServiceImpl) Send(ctx context.Context, req *rpc.SendRequest) (*rpc.SendResponse, error) {
+	getRoomID(chat string) (string, error) {
+		senders := strings.Split(chat, ":")
+		if len(senders) != 2 {
+			return "", fmt.Errorf("invalid Chat ID '%s', should be in the format of user1:user2", chat)
+		}
+
+		sort.Strings(senders)
+		roomID := strings.Join(senders, ":")
+
+		return roomID, nil
+	}
 	resp := rpc.NewSendResponse()
-	resp.Code, resp.Msg = areYouLucky()
+	resp.Code, resp.Messages = 0, "success"
 	return resp, nil
 }
 
+func getRoomID(chat string{}) {
+	
+}
+
 func (s *IMServiceImpl) Pull(ctx context.Context, req *rpc.PullRequest) (*rpc.PullResponse, error) {
+	roomID, err := getRoomID(req.GetChat())
+	if err != nil {
+		return nil, err
+	}
+
+	start := req.GetCursor()
+	end := start + int64(req.GetLimit())
+
+	messages, err := rdb.GetMessagesByRoomID(ctx, roomID, start, end, req.GetReverse())
+	if err != nil {
+		return nil, err
+	}
+
+	respMessages := make([]*rpc.Message, 0)
+	hasMore := len(messages) > int(req.GetLimit())
+	nextCursor := end
+	for _, msg := range messages {
+		temp := &rpc.Message{
+			Chat:     req.GetChat(),
+			Text:     msg.Message,
+			Sender:   msg.Sender,
+			SendTime: msg.Timestamp,
+		}
+		respMessages = append(respMessages, temp)
+	}
+
+	resp := &rpc.PullResponse{
+		Messages:    respMessages,
+		HasMore:     &hasMore,
+		NextCursor:  &nextCursor,
+	}
+	return resp, nil
+
 	resp := rpc.NewPullResponse()
-	resp.Code, resp.Msg = areYouLucky()
+	resp.Code, resp.Messages = 0, "success"
 	return resp, nil
 }
 
